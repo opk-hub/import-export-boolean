ad44925f44b5a040bfc0c6c95b5cdc97a5f45100
praveen <praveenkumaronamala@gamail.com>
2023-03-02 12:07:13 +0000

    commit message

---
+++ export.js
@@ -0,0 +1,2 @@
+const playingCricket = true;
+module.exports = playingCricket;
---
+++ import.js
@@ -0,0 +1,4 @@
+const playingCricket = "./export.js";
+console.log(playingCricket);
+const playingCricket = "./export.js";
+console.log(playingCricket);
